844932e1abe388e552098778008705c78025b5d7
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2021-10-14 11:33:21 +0200

    javascript implemented

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let bund = document.getElementById("bund");
+let topping = document.getElementById("top");
+let antal = document.getElementById("antal");
+
+
+function continueButton(){
+    alert(bund.value + " " + topping.value + " " + antal.value);
+}
