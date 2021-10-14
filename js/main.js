8d8deab51a4efd4b50002c4b9a199553254b56eb
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2021-10-14 18:47:48 +0200

    darkmode

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ let antal = document.getElementById("antal");
 function continueButton(){
     alert(bund.value + " " + topping.value + " " + antal.value);
     document.getElementById("bought-bund").innerHTML = bund.value;
-}
+}   
 
